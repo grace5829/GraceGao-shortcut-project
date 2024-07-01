69087d0d6bbab223eb775318ca8b47c5226b57d0
Grace Gao <grace5829@gmail.com>
2024-07-01 13:09:17 -0400

    adding giphy logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,21 @@ import GifContainer from "@/components/gifContainer";
 import SearchButton from "@/components/searchArea";
 import React, { useState } from "react";
 import { MyObject } from "@/types/common";
+import logo from "./GIPHY-Logo.gif"
+import "./page.css";
 
 export default function Home() {
   const [results, setResults] = useState<MyObject[]>([]);
   const [selectedRating, setSelectedRating] = useState("g");
   return (
-    <div>
+    <div className="container">
       <SearchButton
         setResults={setResults}
         selectedRating={selectedRating}
         setSelectedRating={setSelectedRating}
       />
       <GifContainer results={results} selectedRating={selectedRating} />
+      <img src={logo.src} alt='Giphy-logo' className="giphy-logo"/>
     </div>
   );
 }
